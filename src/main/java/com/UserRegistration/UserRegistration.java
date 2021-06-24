933fffe9e22777b79b138df8ccf339a567c1b1f0
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-24 00:02:35 -0400

    add : Welcome Statement : .

---
+++ src/main/java/com/UserRegistration/UserRegistration.java
@@ -0,0 +1,18 @@
+/***********************************************************************************************
+ *@purpose Implementing User Registration Program .
+ *@author Rekha
+ *@version 1.0
+ *@since 18-06-2021
+ ************************************************************************************************/
+
+package com.UserRegistration;
+
+public class UserRegistration {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome To User Registration");
+		
+	}
+
+}
